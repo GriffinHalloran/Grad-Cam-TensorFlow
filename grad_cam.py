c6c892c998031c59a028642f0d4bf67e5468e3b9
vivianbuan <wl2nw@virginia.edu>
2017-11-25 14:03:51 -0800

    Small bug fix

--- grad_cam.py
+++ grad_cam.py
@@ -13,23 +13,25 @@ class GuidedRelu:
     @ops.RegisterGradient("GuidedRelu")
     def _GuidedReluGrad(op, grad):
         return tf.where(0. < grad, gen_nn_ops._relu_grad(grad, op.outputs[0]), tf.zeros(tf.shape(grad)))
-        
+
 if __name__ == '__main__':
     #-------
     #gradcam
     #-------
     sess = tf.Session()
     imgs = tf.placeholder(tf.float32, [None, 224, 224, 3])
-    vgg = vgg16(imgs, 'vgg16_weights.npz', sess, 242)
-    img1 = imread('cat_dog.jpg', mode='RGB')
+    category = 977
+    img_file = 'Failure Images/failure4.png'
+    vgg = vgg16(imgs, 'vgg16_weights.npz', sess, category)
+    img1 = imread(img_file, mode='RGB')
     img1 = imresize(img1, (224, 224))
-    gradcam = GradCam(vgg.probs, vgg.pool5)
+    gradcam = GradCam(vgg.probs, vgg.pool5, category)
     tf.summary.FileWriter('tf.log', sess.graph)
     ll1 = sess.run([vgg.probs, gradcam], feed_dict={vgg.imgs: [img1]})
     prob = ll1[0][0]
     heatmap = np.array(ll1[1])
-    heatmap = toimage(heatmap)
-    #heatmap = double2image.to_image(heatmap)
+    #heatmap = toimage(heatmap)
+    heatmap = double2image.to_image(heatmap)
     heatmap = imresize(heatmap, (224, 224))
     imsave('gradcam.png', heatmap)
     #---------------
@@ -39,10 +41,10 @@ if __name__ == '__main__':
     with g.gradient_override_map({'Relu': 'GuidedRelu'}):
         sess = tf.Session()
         imgs = tf.placeholder(tf.float32, [None, 224, 224, 3])
-        vgg = vgg16(imgs, 'vgg16_weights.npz', sess, 242)
-        img1 = imread('cat_dog.jpg', mode='RGB')
+        vgg = vgg16(imgs, 'vgg16_weights.npz', sess, category)
+        img1 = imread(img_file, mode='RGB')
         img1 = imresize(img1, (224, 224))
-        guided_backprop = Guided_backprop(vgg.imgs, vgg.probs)
+        guided_backprop = Guided_backprop(vgg.imgs, vgg.probs, category)
         ll = sess.run([vgg.probs, guided_backprop], feed_dict={vgg.imgs: [img1]})
         print ll[1].shape
         guided_backprop = toimage(ll[1])
@@ -51,7 +53,7 @@ if __name__ == '__main__':
         imsave('guided_backprop.png', guided_backprop)
     #---------------
     #Guided gradcam
-    #--------------- 
+    #---------------
     guided_gradcam = ll[1]
     cam = resize(ll1[1]/np.max(ll1[1]), (224, 224), preserve_range = True)
     for i in range(3):
